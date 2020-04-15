a6f905860466c4464629fb0d291ec081c933d555
Michael Yang <themichaelyang@gmail.com>
2020-04-15 03:11:30 -0400

    add tc3 encoding

--- veri_polyg.py
+++ veri_polyg.py
@@ -5,13 +5,14 @@ import time
 # ====== various encodings =====
 
 def polygraph_sat(n, edges, constraints, s): 
-    print(constraints)
     # maybe should just return a list?
     # requires all edges of known RW
     for edge in edges:
         s.add(var(edge))
 
     # requires one but not both of constraints to be satisfied
+    # actually, this is between two groups (either must be true)
+    # must revert the encoding
     for constraint in constraints:
         s.add(Xor(var(constraint[0]), 
                   var(constraint[1])))
@@ -33,35 +34,24 @@ def label(edge):
 def label_aux(edge):
     return 'a' + label(edge)
 
-# n : integer, number of vertices
-# edges : [[i,j], [j,k], ...]
-# choice : [ ([[i,j],[j,k],...], [...]), ...   ]
 def encode_polyg_tc1(n, edges, constraints, s):
-    # acyclicity constraints:
-    # 1) irreflexive
-    for i in range(n):
-        s.add(Not(aux([i, i])))
-
-    # 2) transitive
-    # 3) closure of edges
+    # can't just iterate edges due to constraints also being a possibility as an edge
     for begin in range(n):
+        s.add(Not(aux([begin, begin])))                             # 1) irreflexive
         for end in range(n):
+            s.add(Implies(var([begin, end]), aux([begin, end])))    # 3) closure of edges
             for mid in range(n):
                 connecting = And(aux([begin, mid]), 
                                   aux([mid, end]))
-                s.add(Implies(connecting, aux([begin, end])))
-            s.add(Implies(var([begin, end]), aux([begin, end])))
-
-    # apparently need to include all implies statements? just including the implies of the edges aux is not enough
-    # Probably because of the constraint possible edges!
-    # 3) closure of edges
-    # for edge in edges:
-        # s.add(Implies(var(edge), aux(edge)))
+                s.add(Implies(connecting, aux([begin, end])))       # 2) transitive
 
 def encode_polyg_tc3(n, edges, constraints, s):
-    print("[TODO] your code here; feel free to modify the function signature")
-    assert False
-
+    for begin in range(n):
+        s.add(Not(var([begin, begin])))
+        for end in range(n):
+            for mid in range(n):
+                connecting = And(var([begin, mid]), var([mid, end]))
+                s.add(Implies(connecting, var([begin, end])))
 
 # ====== load file =====
 
@@ -97,14 +87,6 @@ def load_polyg(poly_f):
             str_groups = content.split("|")
             assert len(str_groups) == 2, "ill-format constraints, not two groups"
             con = [[int(g) for g in group.split(',')] for group in str_groups]
-            # con = []
-            # for str_group in str_groups:
-                # group = []
-                # str_edges = str_group.split(";")
-                # assert len(str_edges) >= 1, "ill-format constraints, empty constraint"
-                # for str_edge in str_edges:
-                    # group.append(extract_edge(str_edge))
-                # con.append(group)
             constraints.append(con)
         else:
             print("Line = %s" % line)
@@ -143,8 +125,6 @@ def main(encoding, poly_f):
 
     # (2) solve the above encoding
     print(s.check())
-    # print(s.model())
-
     t3 = time.time()
 
     # print("clause construction: %.fms" % ((t2-t1)*1000))
