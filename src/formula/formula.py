fa907111d59dccafe2df7241731bee85324e09db
Michael Yang <themichaelyang@gmail.com>
2020-08-11 04:04:21 -0400

    distinguish formula and cnf, add cnf formula check

--- src/formula/formula.py
+++ src/formula/formula.py
@@ -4,10 +4,11 @@ def increment_var(current_var):
     # ts-<number>
     return f'ts-{(int(current_var[3:]) + 1)}'
 
-# consider registering instead of using inheritance?
+# TODO: consider registering instead of using inheritance?
+# TODO: could probably convert to_cnf et al to visitor pattern
 class Formula(ABC):
     @abstractmethod
-    def to_cnf(self):
+    def to_cnf_formula(self):
         pass
 
     @abstractmethod
@@ -33,9 +34,9 @@ class UnaryOperator(Formula, ABC):
         inner_expr, inner_var, out_var = self.inner.tseitin(next_var)
 
         subexpr = tseitin_subexpr(inner_var, out_var)
-        cnf = And(inner_expr, subexpr)
+        cnf_formula = And(inner_expr, subexpr)
 
-        return cnf, out, increment_var(out_var)
+        return cnf_formula, out, increment_var(out_var)
 
     @abstractmethod
     def tseitin_subexpr(self, inner_var, out_var):
@@ -56,9 +57,9 @@ class BinaryTseitin:
         r_expr, r_var, out_var = self.right.tseitin(next_var)
 
         subexpr = tseitin_subexpr(l_var, r_var, out_var)
-        cnf = And(l_expr, And(r_expr, subexpr))
+        cnf_formula = And(l_expr, And(r_expr, subexpr))
 
-        return cnf, out_var, increment_var(out_var)
+        return cnf_formula, out_var, increment_var(out_var)
 
     @abstractmethod
     def tseitin_subexpr(self, left_var, right_var, out_var):
@@ -68,7 +69,7 @@ class Atom(Formula):
     def __init__(self, name):
         self.name = name
 
-    def to_cnf(self):
+    def to_cnf_formula(self):
         return self
 
     def tseitin(self, next_var):
@@ -85,24 +86,24 @@ class Expandable(ABC):
     def expand(self):
         pass
 
-    def to_cnf(self):
-        return self.expand().to_cnf()
+    def to_cnf_formula(self):
+        return self.expand().to_cnf_formula()
 
     def tseitin(self):
         return self.expand().tseitin()
 
-def clauses(cnf):
-    if isinstance(cnf, And):
-        for clause in clauses(cnf.left):
+def clauses(cnf_formula):
+    if isinstance(cnf_formula, And):
+        for clause in clauses(cnf_formula.left):
             yield clause
-        for clause in clauses(cnf.right):
+        for clause in clauses(cnf_formula.right):
             yield clause
     else:
-        yield cnf
+        yield cnf_formula
 
 class And(BinaryTseitin, BinaryOperator):
-    def to_cnf(self):
-        return And(self.left.to_cnf(), self.right.to_cnf())
+    def to_cnf_formula(self):
+        return And(self.left.to_cnf_formula(), self.right.to_cnf_formula())
 
     def tseitin_subexpr(self, l_var, r_var, out_var):
         # even AND needs a subexpr, in order to allow for chaining!
@@ -114,18 +115,18 @@ class And(BinaryTseitin, BinaryOperator):
         return subexpr
 
 class Or(BinaryTseitin, BinaryOperator):
-    def to_cnf(self):
-        left_cnf = self.left.to_cnf()
-        right_cnf = self.right.to_cnf()
+    def to_cnf_formula(self):
+        left_cnf_formula = self.left.to_cnf_formula()
+        right_cnf_formula = self.right.to_cnf_formula()
 
         acc = None
         # distribution of <CNF> OR <CNF>
-        for clause_from_left_cnf in clauses(left_cnf):
-            for clause_from_right_cnf in clauses(right_cnf):
+        for clause_from_left_cnf_formula in clauses(left_cnf_formula):
+            for clause_from_right_cnf_formula in clauses(right_cnf_formula):
                 if acc is None:
-                    acc = Or(clause_from_left_cnf, clause_from_right_cnf)
+                    acc = Or(clause_from_left_cnf_formula, clause_from_right_cnf_formula)
                 else:
-                    acc = And(acc, Or(clause_from_left_cnf, clause_from_right_cnf))
+                    acc = And(acc, Or(clause_from_left_cnf_formula, clause_from_right_cnf_formula))
         return acc
 
     def tseitin_subexpr(self, l_var, r_var, out_var):
@@ -137,23 +138,23 @@ class Or(BinaryTseitin, BinaryOperator):
         return subexpr
 
 class Not(UnaryOperator):
-    def to_cnf(self):
+    def to_cnf_formula(self):
         if isinstance(self.inner, Atom):
             return self
         if isinstance(self.inner, Not):
-            return self.inner.to_cnf()
+            return self.inner.to_cnf_formula()
         elif isinstance(self.inner, And):
             left = self.inner.left
             right = self.inner.right
             # DeMorgan's law for AND
-            return Or(Not(left), Not(right)).to_cnf()
+            return Or(Not(left), Not(right)).to_cnf_formula()
         elif isinstance(self.inner, Or):
             left = self.inner.left
             right = self.inner.right
             # DeMorgan's law for OR
-            return And(Not(left), Not(right)).to_cnf()
+            return And(Not(left), Not(right)).to_cnf_formula()
         elif isinstance(self.inner, Expandable):
-            return Not(self.inner.expand()).to_cnf()
+            return Not(self.inner.expand()).to_cnf_formula()
         else:
             raise Exception('unexpected inner!', self.inner)
 
@@ -174,8 +175,33 @@ class Iff(Expandable, BinaryOperator):
         right = self.right
         return Or(And(left, right), And(Not(left), Not(right)))
 
-def to_cnf(formula):
-    return formula.to_cnf()
+def to_cnf_formula(formula):
+    return formula.to_cnf_formula()
 
 def make_atoms(names):
     return (*[Atom(name) for name in names.split()],) # unpack into a tuple!
+
+def is_cnf_formula(formula):
+    if isinstance(formula, And):
+        return is_cnf_formula_BinaryOp(formula, (And, Or))
+    elif isinstance(formula, Or):
+        return is_cnf_formula_BinaryOp(formula, (Or, Not, Atom))
+    elif isinstance(formula, Not):
+        if isinstance(formula.inner, (Not, Atom)):
+            return is_cnf_formula(formula.inner)
+        else:
+            return False
+    elif isinstance(formula, Atom):
+        return True
+
+def is_cnf_formula_BinaryOp(formula, valid_child_instances):
+    is_left_cnf = False
+    if isinstance(formula.left, valid_child_instances):
+        is_left_cnf = is_cnf_formula(formula.left)
+    
+    is_right_cnf = False
+    if isinstance(formula.right, valid_child_instances):
+        is_right_cnf = is_cnf_formula(formula.right)
+        
+    return is_left_cnf and is_right_cnf
+    
